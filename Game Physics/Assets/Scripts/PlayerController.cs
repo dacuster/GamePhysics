453271455d3718ac9c0428fdc71f4c1347596d60
Kyle Murray <kmurr0619@gmail.com>
2019-10-09 23:42:05 -0400

    Made shit look good
    Art folder gots stuff, models in game, game looks good

--- Game Physics/Assets/Scripts/PlayerController.cs
+++ Game Physics/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        particle = GetComponent<Particle2D>();   
+        particle = GetComponent<Particle2D>();
     }
     private void Awake()
     {
