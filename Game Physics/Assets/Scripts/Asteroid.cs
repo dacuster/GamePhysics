95e3dc62a7f526fe172b1f7a996129dedef14620
TheMurrMan <kmurr0619@gmail.com>
2019-10-17 10:53:49 -0400

    Asteroids Have one life

--- Game Physics/Assets/Scripts/Asteroid.cs
+++ Game Physics/Assets/Scripts/Asteroid.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     // Asteroid lives.
-    private int lives = 3;
+    private int lives = 1;
 
 
     // Start is called before the first frame update
